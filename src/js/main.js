c717e63d9c26e25b28bff7c0aca9f5787192cad7
Marta <marta.venturacoelho@studentfr.ch>
2024-12-17 08:52:20 +0100

    Image gallery working with javasxript

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
+// MENU BURGER //
+
 // select all .burgers
 var burgers = document.querySelectorAll(".burger");
 
@@ -15,4 +17,17 @@ burgers.forEach((burger) => {
 
   // Listen for click, start togglemenu function
   burger.addEventListener("click", toggleMenu);
+});
+
+// IMG GALLERY //
+
+// Select all elements with these classes
+var img = document.querySelectorAll(".vin-image-gallery img");
+var main = document.querySelector(".vin-image");
+
+img.forEach((img) => {
+  img.addEventListener("click", () => {
+    var src = img.getAttribute("src");
+    main.setAttribute("src", src);
+  });
 });
